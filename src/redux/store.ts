4272b3c4bb53c66e0383e13fc09136f90cd4bde4
hyper <patrykmleczek.pm@gmail.com>
2023-04-24 01:48:29 +0200

    Remove query property from searchSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "./slice/searchSlice";
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        search: searchReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
