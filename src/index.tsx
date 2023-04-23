f61a532631cdadcfe774a2200d5ed2a75fa8dd19
hyper <patrykmleczek.pm@gmail.com>
2023-04-23 19:55:37 +0200

    Add reset.css reset stylesheet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./style/reset.css";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
