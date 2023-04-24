082c3811b27e9fcd9b4d5149e803e9f34ed34a09
hyper <patrykmleczek.pm@gmail.com>
2023-04-24 02:21:48 +0200

    Update Navbar to use Redux dispatch for listing types

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Navbar.css";
 import {Link} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {setType} from "../../redux/slice/searchSlice";
 
 const Navbar = () => {
+    const dispatch = useDispatch();
+
     return (
         <div className="navbar flex items-center content-between px-3 border-bottom-light-gray">
             <Link to="/">Logo</Link>
             <div className="flex items-center gap-3 weight-500">
-                <button>Buy</button>
-                <button>Rent</button>
+                <button onClick={() => dispatch(setType("buy"))}>Buy</button>
+                <button onClick={() => dispatch(setType("rent"))}>Rent</button>
                 <Link to="/sell">
                     Sell
                 </Link>
