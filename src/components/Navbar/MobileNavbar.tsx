851cca4b91d46596e42e4780fc7c93c6e26d2565
hyper <patrykmleczek.pm@gmail.com>
2023-04-26 11:34:11 +0200

    Add navigation links to MobileNavbar

--- src/components/Navbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar.tsx
@@ -4,9 +4,13 @@ import {Link} from "react-router-dom";
 import useClickOutside from "../../hooks/useClickOutside";
 import MenuIcon from "../icons/MenuIcon";
 import CloseIcon from "../icons/CloseIcon";
+import {useDispatch} from "react-redux";
+import {setType} from "../../redux/slice/searchSlice";
 
 const MobileNavbar = () => {
     const [show, setShow] = useState<boolean>(false);
+
+    const dispatch = useDispatch();
 
     const handleClickOutside = () => {
         setShow(false);
@@ -19,6 +23,11 @@ const MobileNavbar = () => {
         setShow(!show);
     }
 
+    const handleTypeButtonClick = (type: "buy" | "rent") => {
+        setShow(false);
+        dispatch(setType(type));
+    }
+
     return (
         <div ref={ref} className="flex flex-column">
             <div className="navbar-mobile flex items-center content-between border-bottom-light-gray px-2">
@@ -28,8 +37,13 @@ const MobileNavbar = () => {
                 </button>
             </div>
             <div className={show ?
-                "flex flex-column border-bottom-light-gray px-2 absolute navbar-mobile-menu w-full bg-white" :
+                "navbar-mobile-menu flex flex-column border-bottom-light-gray absolute w-full bg-white" :
                 "none"}>
+                <button onClick={() => handleTypeButtonClick("buy")} className="px-2 py-075">Buy</button>
+                <button onClick={() => handleTypeButtonClick("rent")} className="px-2 py-075">Rent</button>
+                <Link className="px-2 py-075" to="/sell">Sell</Link>
+                <Link className="px-2 py-075" to="/login">Sign in</Link>
+                <Link className="px-2 py-075" to="/register">Sign up</Link>
             </div>
         </div>
     );
