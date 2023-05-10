a0b58d3a0ace651f9333fbec9f0f10765c0fc08e
hyper <patrykmleczek.pm@gmail.com>
2023-05-10 14:10:55 +0200

    Update MapDisplay

--- src/components/MapDisplay/MapDisplay.tsx
+++ src/components/MapDisplay/MapDisplay.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./MapDisplay.css";
 import {MapContainer, TileLayer} from "react-leaflet";
 import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
@@ -8,8 +9,8 @@ const MapDisplay = () => {
     const searchState = useSelector((state: RootState) => state.search);
 
     return (
-        <div className="w-half">
-            <MapContainer center={[searchState.lat, searchState.lon]} zoom={13}>
+        <div className="w-half pl-3 pb-1">
+            <MapContainer className="radius-1" center={[searchState.lat, searchState.lon]} zoom={13}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
