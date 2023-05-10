d9a0921751a406788fdfdefa1179abfecfb5c8e8
hyper <patrykmleczek.pm@gmail.com>
2023-05-10 14:16:25 +0200

    Add route for homepage to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import {Provider} from "react-redux";
 import store from "./redux/store";
-import {BrowserRouter, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
+
+const Home = React.lazy(() => import("./components/Home/Home"));
 
 const App = () => {
     return (
         <Provider store={store}>
             <React.Suspense fallback={<LoadingSpinner />}>
                 <BrowserRouter>
-                    <Routes></Routes>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
                 </BrowserRouter>
             </React.Suspense>
         </Provider>
