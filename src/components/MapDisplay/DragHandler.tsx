ad8680732152c076826f614e307ff39d9b61e051
hyper <patrykmleczek.pm@gmail.com>
2023-05-19 00:33:31 +0200

    Update DragHandler

--- src/components/MapDisplay/DragHandler.tsx
+++ src/components/MapDisplay/DragHandler.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import {useMapEvent} from "react-leaflet";
+import {useMapEvents} from "react-leaflet";
 import {useDispatch} from "react-redux";
-import {setCoordinates} from "../../redux/slice/searchSlice";
+import {setCoordinates, setRange} from "../../redux/slice/searchSlice";
 
 const DragHandler = () => {
     const dispatch = useDispatch();
 
-    const map = useMapEvent("dragend", () => {
-        dispatch(setCoordinates([map.getCenter().lat, map.getCenter().lng]));
+    const updateRange = (zoom: number) => {
+        if (zoom < 14) {
+            dispatch(setRange(2_500 + ((14 - zoom) * 2_500)));
+        }
+    }
+
+    useMapEvents({
+        zoomend: event => {
+            updateRange(event.target.getZoom());
+        },
+        dragend: event => {
+            dispatch(setCoordinates([
+                event.target.getCenter().lat,
+                event.target.getCenter().lng
+            ]));
+        },
     });
+
+    // const map = useMapEvent("dragend", () => {
+    //     dispatch(setCoordinates([map.getCenter().lat, map.getCenter().lng]));
+    // });
     return null;
 }
 
